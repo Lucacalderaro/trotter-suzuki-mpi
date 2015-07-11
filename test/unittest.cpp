67a4fcbce526d1f880117f8f232b939d49447bba
Luca Calderaro <calderaroluca@gmail.com>
2015-07-11 23:26:43 +0200

    remove unit

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -18,9 +18,6 @@
  *
  */
 
-#include <cppunit/CompilerOutputter.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/ui/text/TestRunner.h>
 #include <string>
 #include <sstream>
 #include <fstream>
@@ -69,18 +66,7 @@ int main(int argc, char** argv) {
     double h_a = 0.;
     double h_b = 0.;
 
-    // Get the top level suite from the registry
-    CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
-    // Adds the test to the list of test to run
-    CppUnit::TextUi::TestRunner runner;
-    runner.addTest( suite );
-    // Change the default outputter to a compiler error format outputter
-    runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(), std::cerr ) );
-    // Run the tests.
-    bool wasSucessful = runner.run();
-    // Return error code 1 if the one of test failed.
-    if(!wasSucessful)
-        return 1;
+    
 
     int halo_x = (kernel_type == 2 ? 3 : 4);
     int halo_y = 4;
