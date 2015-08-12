121513a2a59475222b29bddef90a2e2fc7e7171f
Luca Calderaro <calderaroluca@gmail.com>
2015-08-12 10:55:39 +0200

    Create cpu_mnode_mtask.sh

---
+++ examples/cpu_mnode_mtask.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+# @ job_name= prova
+# @ initialdir= .
+# @ output= prova.out
+# @ error= prova.err
+# @ total_tasks= 6
+# @ cpus_per_task= 1
+# @ tasks_per_node= 2
+# @ wall_clock_limit= 00:10:00
+srun ./exponential_initial_state > prova.out
